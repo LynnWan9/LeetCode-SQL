7f4ada3cf2fff6c9879cc7db3d69bfcd0fd2407c
LynnWan9 <58793617+LynnWan9@users.noreply.github.com>
2021-01-18 16:48:46 -0700

    Update 1083_Sales_Analysis_II.sql

--- 1083_Sales_Analysis_II.sql
+++ 1083_Sales_Analysis_II.sql
@@ -8,3 +8,10 @@ WHERE product_name = "S8"
         JOIN Product USING (product_id)
         WHERE product_name = "iPhone"
         );
+
+SELECT DISTINCT buyer_id
+FROM Sales
+JOIN Product USING (product_id)
+GROUP BY buyer_id
+HAVING SUM(CASE WHEN product_name = 'S8' THEN 1 ELSE 0 END) > 0
+    AND SUM(CASE WHEN product_name = 'iPhone' THEN 1 ELSE 0 END) = 0;
