c4f325666fdf66501b79afbb6a921b7bea9480e7
LynnWan9 <58793617+LynnWan9@users.noreply.github.com>
2021-01-18 17:29:09 -0700

    Create 1142_User_Activity_for_the_Past_30_Days_II.sql

---
+++ 1142_User_Activity_for_the_Past_30_Days_II.sql
@@ -0,0 +1,3 @@
+SELECT ROUND(IFNULL(COUNT(DISTINCT session_id) / COUNT(DISTINCT user_id), 0), 2) AS average_sessions_per_user
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27';
