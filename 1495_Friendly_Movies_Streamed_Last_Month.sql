c68e0ce69e5444c1d26eba6061e55f74b4d0bb04
LynnWan9 <58793617+LynnWan9@users.noreply.github.com>
2021-01-19 15:16:37 -0700

    Create 1495_Friendly_Movies_Streamed_Last_Month.sql

---
+++ 1495_Friendly_Movies_Streamed_Last_Month.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT c.title
+FROM TVProgram t
+JOIN Content c
+    ON t.content_id = c.content_id
+WHERE Kids_content = 'Y'
+    AND content_type = 'Movies'
+    AND YEAR(program_date) = 2020
+    AND MONTH(program_date) = 6;
